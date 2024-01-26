b2f2c2f2f48687cc7ca2cd95c8c9617bf5beaebb
Sriram Kavi <21A91A04L1@aec.edu.in>
2024-01-26 22:19:12 +0530

    Time: 47 ms (89.51%) | Memory: 16.7 MB (60.91%) - LeetSync

---
+++ 39-combination-sum/combination-sum.py
@@ -0,0 +1,23 @@
+class Solution:
+    def combination_Sum(self, ind, arr, n, k, ds, res):
+        if ind == n:
+            if k == 0:
+                a = ds.copy()
+                res.append(a)
+            return
+        if arr[ind] <= k:
+            ds.append(arr[ind])
+            k = k - arr[ind]
+            self.combination_Sum(ind, arr, n, k, ds, res)
+            k = k + arr[ind]
+            ds.pop()
+        self.combination_Sum(ind + 1, arr, n, k, ds, res)
+        return res
+
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        ds = []
+        res = []
+        self.combination_Sum(0, candidates, len(candidates), target, ds, res)
+        print(res)
+        return res
+        
