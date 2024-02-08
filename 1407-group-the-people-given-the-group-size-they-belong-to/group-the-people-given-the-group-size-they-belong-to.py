d26543af4f9622656a587e9e0bf722a3243a9273
Sriram Kavi <21A91A04L1@aec.edu.in>
2024-02-08 18:33:36 +0530

    Time: 66 ms (66.41%) | Memory: 16.7 MB (59.16%) - LeetSync

---
+++ 1407-group-the-people-given-the-group-size-they-belong-to/group-the-people-given-the-group-size-they-belong-to.py
@@ -0,0 +1,17 @@
+class Solution:
+    def groupThePeople(self, arr: List[int]) -> List[List[int]]:
+        d = {}
+        l = []
+        for i in range(len(arr)):
+            if arr[i] in d and len(d[arr[i]]) == arr[i]:
+                l.append(d[arr[i]])
+                d[arr[i]] = [i]
+            elif arr[i] in d and len(d[arr[i]]) <= arr[i]:
+                d[arr[i]].append(i)
+            else:
+                d[arr[i]] = [i]
+        for k, v in d.items():
+            l.append(v)
+        l.sort()
+        return l
+        
