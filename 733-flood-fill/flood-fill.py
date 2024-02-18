19014580f288866b3f38f9ec79af767bba2e78ed
Sriram Kavi <21A91A04L1@aec.edu.in>
2024-02-18 20:39:38 +0530

    Time: 58 ms (95.73%) | Memory: 16.6 MB (87.27%) - LeetSync

---
+++ 733-flood-fill/flood-fill.py
@@ -0,0 +1,21 @@
+class Solution:
+    def floodfill(self, i, j, image, color, m, n, vis, d):
+        if i >= m or j >= n or i < 0 or j < 0 or vis[i][j] == 1:
+            return
+        vis[i][j] = 1
+        if image[i][j] == d:
+            image[i][j] = color
+            self.floodfill(i + 1, j, image, color, m, n, vis, d)
+            self.floodfill(i, j + 1, image, color, m, n, vis, d)
+            self.floodfill(i- 1, j, image, color, m, n, vis, d)
+            self.floodfill(i, j - 1, image, color, m, n, vis, d)
+
+
+
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
+        m = len(image)
+        n = len(image[0])
+        vis = [[0] * (n) for _ in range(m)]
+        d = image[sr][sc]
+        self.floodfill(sr, sc, image, color, m, n, vis, d)
+        return image
