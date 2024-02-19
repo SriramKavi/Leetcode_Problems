7de45ee92a5e5a23c02b5caf76b9355ebb20570c
Sriram Kavi <21A91A04L1@aec.edu.in>
2024-02-19 23:30:24 +0530

    Time: 79 ms (36.41%) | Memory: 18 MB (94.58%) - LeetSync

--- 813-all-paths-from-source-to-target/all-paths-from-source-to-target.py
+++ 813-all-paths-from-source-to-target/all-paths-from-source-to-target.py
@@ -1,26 +1,21 @@
 class Solution:
     def dfs(self, node, adj, vis, ds, V):
         if node == V - 1:
-            d = ds.copy()
-            l.append(ds)
+            if ds[0] == 0:
+                d = ds.copy()
+                l.append(ds)
             return
-        vis[node] = 1
         for i in adj[node]:
-            self.dfs(i, adj, vis, ds + [i], V)
-            vis[i] = 1
+            if vis[i] != 1:
+                vis[i] = 1
+                self.dfs(i, adj, vis, ds + [i], V)
+                vis[i] = 0
 
     def allPathsSourceTarget(self, adj: List[List[int]]) -> List[List[int]]:
-        if adj==[[2],[2],[]]:
-            return [[0,2]]
         V = len(adj)
         vis = [0 for _ in range(V)]
         global l
         l = []
         ds = []
-        for i in range(V):
-            if vis[i] != 1:
-                ds += [i]
-                self.dfs(i, adj, vis, ds, V)
+        self.dfs(0, adj, vis, ds + [0], V)
         return l
-
-        
