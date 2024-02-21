e6e5e343da322db58e20239401faf7075ec706ae
Sriram Kavi <21A91A04L1@aec.edu.in>
2024-02-21 23:42:09 +0530

    Time: 6 ms (66.15%) | Memory: 8.9 MB (8.30%) - LeetSync

--- 201-bitwise-and-of-numbers-range/bitwise-and-of-numbers-range.cpp
+++ 201-bitwise-and-of-numbers-range/bitwise-and-of-numbers-range.cpp
@@ -1,32 +1,7 @@
 class Solution {
 public:
     int rangeBitwiseAnd(int left, int right) {
-        int low = 0, ind = 0, high = 0;
-        for(int i = 31; i >= 0; i--){
-            if((left & 1 << i) > 0){
-                low = 1 << i;
-                ind = i;
-                break;
-            }
-        }
-        if(ind == 30){
-            high = 2147483647;
-        }
-        else{
-            high = (1 << (ind + 1));
-        }
-        int res = left;
-        if(right <= high){
-            for(int j = left; j <= right; j++){
-                if(j < 2147483647){
-                    res = res & j;
-                }
-                else{
-                    break;
-                }
-            }
-            return res;
-        }
-        return 0;
+        while(right > left) right = right & (right - 1);
+        return left & right;
     }
 };
