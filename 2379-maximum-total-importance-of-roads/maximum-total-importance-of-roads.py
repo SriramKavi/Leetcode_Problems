35bb8a8058240def4a84e3029517d8356cecde20
Sriram Kavi <21A91A04L1@aec.edu.in>
2024-06-28 13:33:03 +0530

    Time: 1205 ms (79.10%) | Memory: 41.4 MB (90.57%) - LeetSync

--- 2379-maximum-total-importance-of-roads/maximum-total-importance-of-roads.py
+++ 2379-maximum-total-importance-of-roads/maximum-total-importance-of-roads.py
@@ -1,26 +1,13 @@
 class Solution:
     def maximumImportance(self, n: int, roads: List[List[int]]) -> int:
-        d = {}
-        for city in roads:
-            if city[0] in d and city[1] not in d:
-                d[city[0]] += 1
-                d[city[1]] = 1
-            elif city[1] in d and city[0] not in d:
-                d[city[1]] += 1
-                d[city[0]] = 1
-            elif city[0] in d and city[1] in d:
-                d[city[0]] += 1
-                d[city[1]] += 1
-            else:
-                d[city[0]] = 1
-                d[city[1]] = 1
-        lst = sorted(d.items(), key = lambda x: (x[1], x[0]), reverse = True)
-        res = [0] * n
-        p = n
-        for i in lst:
-            res[i[0]] = p
-            p -= 1
-        ans = 0
-        for cit in roads:
-            ans += res[cit[0]] + res[cit[1]]
-        return ans
+        arr = [0] * n
+        for a, b in roads:
+            arr[a] += 1
+            arr[b] += 1
+        print(arr)
+        arr.sort()
+        print(arr)
+        res = 0
+        for i in range(n):
+            res += arr[i] * (i + 1)
+        return res
