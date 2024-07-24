dd98bb226dd24c0e9f1747b361e48b6f4e3fb5fb
Sriram Kavi <21A91A04L1@aec.edu.in>
2024-07-24 02:04:25 -0400

    Time: 941 ms (81.52%) | Memory: 22.9 MB (78.26%) - LeetSync

--- 1333-sort-the-jumbled-numbers/sort-the-jumbled-numbers.py
+++ 1333-sort-the-jumbled-numbers/sort-the-jumbled-numbers.py
@@ -1,25 +1,16 @@
 class Solution:
-    def map(self, n, mapping):
-        s = 0
-        i = 0
-        if n == 0:
-            return mapping[n]
-        while n:
-            r = n % 10
-            r = mapping[r]
-            s += r * pow(10, i)
-            i += 1
-            n //= 10
-        return s 
-
-
     def sortJumbled(self, mapping: List[int], nums: List[int]) -> List[int]:
-        mapped = []
-        for i in range(len(nums)):
-            mapped.append((self.map(nums[i], mapping), i))
-        x = lambda a: (a[0], a[1])
-        ans = sorted(mapped, key = x)
-        res = []
-        for k, v in ans:
-            res.append(nums[v])
-        return res
+        def map(n):
+            s = 0
+            i = 0
+            if n == 0:
+                return mapping[n]
+            while n:
+                r = n % 10
+                r = mapping[r]
+                s += r * pow(10, i)
+                i += 1
+                n //= 10
+            return s 
+        ans = sorted(nums, key = lambda p: map(p))
+        return ans
